3e47c646033478c80dd701193b38042394f37a0c
Sripathi Krishnan <sripathi.krishnan@hashedin.com>
2020-05-22 11:03:39 +0530

    Adding support for Flask

--- squealy-core/squealy/flask.py
+++ squealy-core/squealy/flask.py
@@ -1,54 +1,71 @@
-from squealy import Squealy
-from .engines import Engine
-from .loader import load_objects
+from flask import request, current_app
+from flask.views import MethodView
+from sqlalchemy import create_engine
+from squealy import Squealy, Engine, Table, SquealyConfigException
 
 class FlaskSquealy(Squealy):
-    pass
+    def __init__(self, app, home_dir=None, snippets=None, resources=None):
+        super(FlaskSquealy, self).__init__(snippets=snippets, resources=resources)
+        if home_dir:
+            self.load_resources(home_dir)
+        self._load_engines(app)
 
-class FlaskSqlAlchemyEngineFactory(EngineFactory):
-    '''Yes, it does look like a Java/Spring developer wrote this class 
-
-        This class assumes the following - 
-        1) A Flask based application
-        2) SQLAlchemy ORM
-        3) Flask config object has SQLALCHEMY_DATABASE_URI defined, and is a valid database URI
-        4) In case of multiple databases, SQLALCHEMY_BINDS is defined, and is a dictionary
-    '''
-    def get_engine(self, engine_name):
-        from flask import current_app
-        from sqlalchemy import create_engine
-
-        if not engine_name or engine_name == 'default':
-            if 'SQLALCHEMY_DATABASE_URI' in current_app.config:
-                engine = create_engine(current_app.config['SQLALCHEMY_DATABASE_URI'])
-            else:
-                raise SquealyException("Cannot load engine 'default'. Missing configuration SQLALCHEMY_DATABASE_URI")
-        else:
-            binds = current_app.config.get('SQLALCHEMY_BINDS', {})
-            if engine_name in binds:
-                conn_url = binds[engine_name]
-                engine = create_engine(conn_url)
-            else:
-                raise raise SquealyException("Cannot load engine '" + engine_name + ', did you configure SQLALCHEMY_BINDS?")
+        # Store squealy under app['extensions'] so we can later retrieve it
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['squealy'] = self
+    
+    def _load_engines(self, app):
+        if 'SQLALCHEMY_DATABASE_URI' in app.config:
+            engine = create_engine(self.app.config['SQLALCHEMY_DATABASE_URI'])
+            self.add_engine('default', SqlAlchemyEngine(engine))
         
-        if not engine:
-            raise SquealyException("Did not expect engine to be null at this point")
-
-        return SqlAlchemyEngine(engine)
-
+        binds = app.config.get('SQLALCHEMY_BINDS', {})
+        for name, database_uri in binds.items():
+            engine = create_engine(database_uri)
+            self.add_engine(name, SqlAlchemyEngine(engine))
 
 class SqlAlchemyEngine(Engine):
     def __init__(self, engine):
         self.engine = engine
+        self.param_style = 'qmark'
     
     def execute(self, query, bind_params):
         with self.engine.connect() as conn:
-            result = conn.execute(finalquery, bindparams)
+            result = conn.execute(query, bind_params)
             cols = result.keys()
             rows = result.fetchall()
         table = Table(columns=result.keys(), data=[r.values() for r in rows])
+        return table
 
     def execute_for_json(self, query, bind_params):
         with self.engine.connect() as conn:
             result = conn.execute(finalquery, bindparams)
             return result.fetchone()[0]
+
+class SqlView(MethodView):
+    def __init__(self, resource_id=None, resource=None):
+        if resource:
+            self.resource = resource
+        elif resource_id:
+            squealy = current_app.extensions['squealy']
+            self.resource = squealy.get_resource(resource_id)
+        else:
+            self.resource = None
+        
+    def build_context(self, request, *args, **kwargs):
+        params = request.args.to_dict()
+        params.update(kwargs)
+
+        return {
+            "params": params
+        }
+
+    def get(self, *args, **kwargs):
+        squealy = current_app.extensions['squealy']
+        if not self.resource:
+            self.resource = squealy.get_resource(request.endpoint)
+        
+        context = self.build_context(request, *args, **kwargs)
+        data = self.resource.process(squealy, context)
+        return data
