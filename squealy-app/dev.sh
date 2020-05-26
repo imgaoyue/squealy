03c50b987d038be9e8c20016e3fea4a3aa15481d
Sripathi Krishnan <sripathi.krishnan@hashedin.com>
2020-05-26 12:53:36 +0530

    Adding resources for various use cases

--- squealy-app/dev.sh
+++ squealy-app/dev.sh
@@ -1,1 +1,4 @@
-SQUEALY_CONFIG_FILE=squealy-home/config.yml FLASK_APP=squealyapp FLASK_ENV=development flask run
+SQUEALY_BASE_DIR=squealy-home
+FILES_TO_WATCH=$(find $SQUEALY_BASE_DIR -type f -name "*.yml" | tr '\n' ':' | rev | cut -c2- | rev)
+pip install -r requirements.txt
+SQUEALY_CONFIG_FILE=squealy-home/config.yml FLASK_APP=squealyapp FLASK_ENV=development flask run --extra-files=$FILES_TO_WATCH
